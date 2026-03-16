99ad1ff2cc43499018a0cbff355200d58cb73d8a
Akari <akari.schaarschmidt@student.onslow.school.nz>
2026-03-16 23:46:24 +0000

    finished functions: returns and paramaters task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,10 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        let length: Double = 6
+        let width: Double = 4.5
+        let height: Double = 2.7
+
         func printRoomIntro() {
         print("This program calculates room area and volume.")
         print("It also finds the usable space after furniture.")
@@ -28,6 +32,36 @@ struct SwiftPlayground {
             print("Done. Thanks for using the calculator.")
         }
         printGoodbye()
+        
+        func roomVolume() -> Double {
+            // return volume using stored constants
+            length * width * height
+        }
+        
+        func printArea(length: Double, width: Double) {
+            // calculate and print area
+            print(length * width, "m²")
+        }
+        // confirm roomVolume function works
+        roomVolume()
+        print(roomVolume(), "m³")
+        
+        // confirm printArea function works
+        printArea(length: length, width: width)
+        
+        func area(length: Double, width: Double) -> Double {
+            // return area
+            length * width
+        }
+        // confirm area function works
+        print(area(length: length, width: width))
+        
+        func volume(length: Double, width: Double, height: Double) -> Double {
+            // returns volume
+            length * width * height
+        }
+        // confirm volume function works
+        print(volume(length: length, width: width, height: height))
     }
 }
 
