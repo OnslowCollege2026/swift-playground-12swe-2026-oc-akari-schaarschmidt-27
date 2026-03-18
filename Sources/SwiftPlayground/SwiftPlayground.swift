f3f36856a9e76e34585073d7a6c875b230952558
Akari <akari.schaarschmidt@student.onslow.school.nz>
2026-03-18 21:06:50 +0000

    scope task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,55 +4,40 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let length: Double = 6
-        let width: Double = 4.5
-        let height: Double = 2.7
         
-        /*
-        Calculates the area of a room
-        - Parameters:
-        --- length: the length of the room in metres
-        --- width: the width of the room in metres
-        -Returns: Calculated area of the room as a double
-        */
-        func roomArea(length: Double, width: Double) -> Double {
-            // return area
-            length * width
+        if true {
+            let note = "i am"
+            print(note)
         }
-        /*
-        Calculates the volume of a room
-        - Parameters:
-        ---length: the length of the room in metres
-        ---width: the width of the room in metres
-        ---height: the height of the room in metres
-        Returns: Calculated volume of the room
-        */
-        func roomVolume() -> Double {
-            // return volume using stored constants
-            length * width * height
-        }
-        /*
-        Checks if a room is small
-        - Parameters
-        --- volume: the volume of the room in m³
-        -Boolean: a variable that is either true or false
-        Returns: true if the room is under 60 m³, false if it is over 60 m³
-        */
-        func isRoomSmall(volume: Double) -> Bool {
-            volume < 60
-        }
+        //print(note)
+        
+        var status = ""
 
-        /*
-        Reduces the height of a room by a percentage
-        - Parameters:
-        --- original: The original height of the room in meters
-        --- reductionPercent: The percentage that the original height is being reduced by
-        Returns: Reduced value in meters
-        */
-        func reducedHeight(original: Double, reductionPercent: Double) -> Double {
-            let multiplier = (100.0 - reductionPercent) / 100.0
-            return original * multiplier
+        if true {
+            status = "sphere"
         }
+        print(status)
+        
+        func showArea() {
+            let value1: Double = 6
+            let value2: Double = 4.5
+            let value3: Double = 2.7
+            print(value1 * value2 * value3)
+        }
+        //print(value1 * value2 * value3)
+
+        let isLarge = false
+
+        if isLarge == false {
+            let label: String = "false"
+            print(label)
+        } else {
+            let label2: String = "true"
+            print(label2)
+        }
+        //print(label)
+        //print(label2)
+
     }
 }
 
