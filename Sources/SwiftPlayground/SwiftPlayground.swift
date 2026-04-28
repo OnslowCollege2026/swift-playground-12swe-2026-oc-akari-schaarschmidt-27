03c743c0c2ba64f77ac88e2112cc53e1395af28d
Akari <akari.schaarschmidt@student.onslow.school.nz>
2026-04-28 01:14:51 +0000

    assesment wip

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -16,11 +16,12 @@ struct SwiftPlayground {
         let maxKilosPerBag: Double = 5.0 // the maximum amount of kilograms per bag is 5 kilograms/5000 grams
         
         //  - Prices
-        let pricePerKilo: Double = 3.00 // price of kumara per kilo
+        let kiloPrice: Double = 3.00 // price of kumara per kilo
         let bagPrice: Double = 0.20 // price for bags 20 cents     
         
         // Variables
         var stock: Double = 0.0 // the current stock of kumara in grams
+        var currentMoney: Double = 0.0
         
         // bin can fit 50 kilos of kumara and there are 5000 bags
 
@@ -38,32 +39,85 @@ struct SwiftPlayground {
             print("")
             print("Info")
             print("_. View history")
+            divider()
+            if let userInput = readLine(), let selection = Int(userInput) {
+                if selection == 1 {
+                    divider()
+                    addStock()
+                }
+                if selection == 2 {
+                    divider()
+                    viewStock()
+                    menu()
+                }
+                if selection == 3 {
+                    sell()
+                }
+                if selection == 4 {
+                    
+                }
+                if selection == 5 {
+                    
+                }
+                if selection == 6 {
+                    
+                }
 
+            }
         }
-
-
+        // initial call of menu function
+        menu()
+        // function to add stock to the kumara shop
         func addStock() {
             print("How much Kumara would you like to add in Kilograms?")
-            
+            // gets user input for how much kumara to add
             if let userInput = readLine(), let amount = Double(userInput) {
+                // checks if the inputed amount is valid (>100g and < the bin limit which is 50 kilograms)
                 if amount >= 0.1 && amount <= binLimit {
+                    //checks to make sure that adding the inputed amount doesnt cause the bin to go over the bin limit (50 kilograns)
                     if stock + amount <= binLimit{
+                        // adds the inputed amount of kumara to the stock
                         stock += amount
+                        divider()
                         print(stock, "Kilograms of Kumara in stock")
+                        menu()
                     } else {
+                        // prints an error if the inputed value was over 50 kilograms and asks again for user input
                         print("Stock bin has a limit of 50 Kilograms, please choose a lower number")
                         print(stock, "Kilograms of Kumara currently in stock")
                         addStock()
                     }
                     } else {
+                        // pritns an error if the input was invalid and asks again for user input
                         print("Invalid Number, Please choose a number from 1 to 50")
                         addStock()
                 }
             }
         }
-        addStock()
+        
         func viewStock() {
             print(stock, "Kilograms of Kumara currently in stock")
+        }
+        func sell() {
+            print("How much kumara are you selling in kilograms?")
+            if let userInput = readLine(), let amount = Double(userInput) {
+                if amount <= stock && amount >= 0.1 {
+                    var moneyMade: Double = amount * kiloPrice
+                    let bags: Double = amount / 5
+                    var roundUpBags: Int = Int(bags)
+                    if bags > Double(roundUpBags) {
+                        roundUpBags += 1
+                    }
+                    print(roundUpBags)
+                    moneyMade += Double(roundUpBags) * bagPrice
+                    print(moneyMade)
+                    stock - amount
+                    /// ====================================================== do money stuff add money to the currentmoney variable ighnisfnhigjdfgjkdnbv
+                } else {
+                    print("Invalid Number, please choose a number from 0.1 - 50.0")
+                    sell()
+                }
+            }
         }
     }
 }
